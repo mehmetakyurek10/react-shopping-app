4eff8ec32bf37e65f0a3b22372555d9b368ee8af
mehmetakyurek1 <151149491+mehmetakyurek10@users.noreply.github.com>
2026-03-14 21:58:59 +0300

    feat: add clothing, electronics, and jewelery routes to homepage

--- src/link/productLink/Electronics.jsx
+++ src/link/productLink/Electronics.jsx
@@ -1,5 +1,49 @@
+import { useSelector } from "react-redux";
 import "../../css/Electronics.css";
 
 export default function Electronics() {
-  return <></>;
+  const products = useSelector((state) => state.products.products);
+
+  //console.log(products);
+
+  const monitors = products
+    .filter((urun) => urun.title && urun.category === "electronics")
+    .slice(4);
+
+  const ssdHdd = products
+    .filter((urun, index) => urun.title && urun.category === "electronics")
+    .slice(0, 3);
+  return (
+    <>
+      <div className="home-triple-div">
+        <h3 style={{ textAlign: "center" }}>Monitörler</h3>
+        <div className="highlights-container">
+          {monitors.map((urun) => (
+            <div className="highlights-product-card">
+              <img
+                src={urun.image}
+                alt={urun.title}
+                className="product-image"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="home-triple-div">
+        <h3 style={{ textAlign: "center" }}>SSD & HDD</h3>
+        <div className="highlights-container">
+          {ssdHdd.map((urun) => (
+            <div className="highlights-product-card">
+              <img
+                src={urun.image}
+                alt={urun.title}
+                className="product-image"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/link/productLink/Jewelery.jsx
+++ src/link/productLink/Jewelery.jsx
@@ -1,5 +1,30 @@
+import { useSelector } from "react-redux";
 import "../../css/Jewelery.css";
 
 export default function Jewelery() {
-  return <></>;
+  const products = useSelector((state) => state.products.products);
+  console.log(products);
+
+  const jewelry = products.filter(
+    (urun) => urun.title && urun.category === "jewelery",
+  );
+
+  return (
+    <>
+      <div className="home-triple-div">
+        <h3 style={{ textAlign: "center" }}>Mücevherler</h3>
+        <div className="highlights-container">
+          {jewelry.map((urun) => (
+            <div className="highlights-product-card">
+              <img
+                src={urun.image}
+                alt={urun.title}
+                className="product-image"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
